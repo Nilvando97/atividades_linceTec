e77e0606fff7159f41d50dac99e4ad1aebc953e2
Nilvando <nilvandopolicarpo@gmail.com>
2024-03-22 15:37:28 -0300

    Lista aleatória

--- ap2_colecoes/ap1.dart
+++ ap2_colecoes/ap1.dart
@@ -1,23 +1,13 @@
-// import 'dart:math';
-
-// void main() {
-//   // Criar gerador de números
-//   final random = Random();
-
-//   // Utilizando o construtor [List.generate] para criar a lista
-//   final lista = List.generate(10, (_) => random.nextInt(100));
-
-//   // Imprimindo a lista conforme especificado
-//   for (var indice = 0; indice < lista.length; indice++) {
-//     print('Posicao: $indice, Valor: ${lista[indice]}');
-//   }
-// }
 import 'dart:math';
 
 void main() {
+  // Criando um objeto da classe Random para gerar números aleatórios
   final random = Random();
+
+  // Criando uma lista com 10 elementos, cada um sendo um número aleatório entre 0 e 100
   final lista = List.generate(10, (index) => random.nextInt(101));
 
+  // Iterando sobre cada elemento da lista e imprimindo a posição e o valor do elemento
   for (var i = 0; i < lista.length; i++) {
     print('Posição: $i, valor: ${lista[i]}');
   }
