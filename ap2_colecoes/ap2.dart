27cc690bec882d7b93e7b36fafe5b621f402c522
Nilvando <nilvandopolicarpo@gmail.com>
2024-03-23 19:51:32 -0300

    Utilizando métodos de lista #2

--- ap2_colecoes/ap2.dart
+++ ap2_colecoes/ap2.dart
@@ -5,8 +5,8 @@ void main() {
   // Criando uma instância de Random para gerar números aleatórios
   final random = Random();
 
-  // Gerando uma lista com 50 números aleatórios entre 0 e 14
-  final lista = List.generate(50, (_) => random.nextInt(15));
+  // Gerando uma lista com 50 números aleatórios entre 0 e 15
+  final lista = List.generate(50, (_) => random.nextInt(16));
 
   // Imprimindo a lista original
   print("Lista original: ${lista.join(' ; ')}");
---
+++ ap2_colecoes/ap3.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+void main(){
+  // Cria uma instância de Random para gerar números aleatórios
+  final random = Random();
+
+  // Cria uma lista com 50 números aleatórios entre 10 e 21
+   final lista = List.generate(50, (_) => random.nextInt(12) + 10);
+
+   // Imprime a lista original
+   print("Lista original: ${lista.join(' ; ')}");
+
+   // Converte a lista em um conjunto para pegar os itens únicos
+   final itensUnicos = lista.toSet();
+
+   // Imprime os itens únicos
+   print("Lista original: ${itensUnicos.join(' ; ')}");
+}
