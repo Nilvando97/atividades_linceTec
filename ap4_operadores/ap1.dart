3d3a75dc5c051a59d3f8b1f1bcc1cc9a31ba014d
Nilvando <nilvandopolicarpo@gmail.com>
2024-03-25 09:09:53 -0300

    Anos bissextos

--- ap4_operadores/ap1.dart
+++ ap4_operadores/ap1.dart
@@ -1,3 +1,4 @@
+// Função que verifica se um ano é bissexto
 bool ehAnoBissexto(int ano) {
   if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
     return true;
@@ -5,10 +6,14 @@ bool ehAnoBissexto(int ano) {
     return false;
   }
 }
+
 void main() {
+  // Lista de anos a serem verificados
   List<int> anos = [2016, 1988, 2000, 2100, 2300, 1993];
 
+  // Loop para verificar cada ano na lista
   for (int ano in anos) {
+    // Verifica se o ano é bissexto usando a função ehAnoBissexto
     if (ehAnoBissexto(ano)) {
       print("$ano é um ano bissexto.");
     } else {
