ba7a02437d3f24fca6bfb6b133ce7d0ae2b51519
Nilvando <nilvandopolicarpo@gmail.com>
2024-04-01 13:09:17 -0300

    Área de Retângulo

--- ap7_oop1/ap2.dart
+++ ap7_oop1/ap2.dart
@@ -1,19 +1,25 @@
-class Retangulo{
-  double larura;
-  double altura;
+// Definição da classe Retangulo
+class Retangulo {
+  double largura; // Atributo para a largura do retângulo
+  double altura; // Atributo para a altura do retângulo
 
-  Retangulo(this.larura, this.altura);
+  // Construtor da classe Retangulo, que recebe a largura e altura como parâmetros e inicializa os atributos
+  Retangulo(this.largura, this.altura);
 
-  double calcularArea(){
-    return larura * altura;
+  // Método para calcular a área do retângulo
+  double calcularArea() {
+    return largura * altura; // Retorna a multiplicação da largura pela altura
   }
 }
 
-void main(){
-Retangulo retangulo = Retangulo(7.0, 4.0);
+// Função principal do programa
+void main() {
+  // Criação de uma instância da classe Retangulo com largura 7.0 e altura 4.0
+  Retangulo retangulo = Retangulo(7.0, 4.0);
 
-double area = retangulo.calcularArea();
+  // Chamada do método calcularArea para obter a área do retângulo
+  double area = retangulo.calcularArea();
 
-print('Area do retângulo: $area');
-
+  // Impressão da área do retângulo no console
+  print('Area do retângulo: $area');
 }
