70dcd320d1baba85cfb0cd41885af1a3a2170626
Nilvando <nilvandopolicarpo@gmail.com>
2024-04-01 20:53:40 -0300

    Gêneros Musicais

--- ap8_oop2/ap1.dart
+++ ap8_oop2/ap1.dart
@@ -1,5 +1,6 @@
-import 'dart:math';
+import 'dart:math'; // Importa a classe Random para gerar números aleatórios
 
+// Enumerador para representar diferentes gêneros musicais
 enum GeneroMusical {
   Rock,
   Pop,
@@ -9,12 +10,13 @@ enum GeneroMusical {
 }
 
 void main() {
-  var random = Random();
-  var generoIndex = random.nextInt(GeneroMusical.values.length);
-  var generoEscolhido = GeneroMusical.values[generoIndex];
+  var random = Random(); // Cria uma instância da classe Random
+  var generoIndex = random.nextInt(GeneroMusical.values.length); // Gera um índice aleatório baseado no tamanho do enum
+  var generoEscolhido = GeneroMusical.values[generoIndex]; // Obtém o gênero musical correspondente ao índice aleatório
 
-  String generoMusical;
+  String generoMusical; // Variável para armazenar o nome do gênero musical escolhido
 
+  // Switch para identificar o gênero musical escolhido e atribuir o nome correspondente à variável generoMusical
   switch (generoEscolhido) {
     case GeneroMusical.Rock:
       generoMusical = 'Rock';
@@ -35,5 +37,6 @@ void main() {
       generoMusical = 'Desconhecido';
   }
 
+  // Imprime o gênero musical escolhido no console
   print('Meu gênero musical preferido é $generoMusical');
 }
